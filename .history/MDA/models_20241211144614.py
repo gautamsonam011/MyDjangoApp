d4c069b4a06586985b53ff3317fa20ce493c039c
Sonam Gautam <gautamsonam011@gmail.com>
2024-12-11 14:50:38 +0530

    first commit

---
+++ .history/MDA/models_20241211144614.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class Member(models.Model):
+    firstName = models.CharField(max_length=240)
+    lastName = models.CharField(max_length=240)
+    age = models.CharField(max_length=240)
+    mobile = models.CharField(max_length=12)
+    joined_date = models.DateField()
